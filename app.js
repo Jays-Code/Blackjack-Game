d562d7150609f53491e165c7c803d399fa3879c9
Jays-Code <dfbfbc@gmail.com>
2019-04-11 16:12:00 -0400

    updated code for card deck

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
             
 //Card Deck creation help http://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
 
-let deck = new Array();
+let deck = []
 let cardSuits= ["spades", "diamonds", "clubs", "hearts"];
 let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 //let cardValues = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
@@ -10,32 +10,63 @@ let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "
    
 function getDeck () 
 {
-    let deck = new Array();
-    for (i = 0; i < cardSuits.length; i++) 
+    let newDeck = []
+    //let deck = new Array();
+    for (let i = 0; i < cardSuits.length; i++) 
     {
-        for (x = 0; x < cardValues.length; x++) 
+        for (let x = 0; x < cardValues.length; x++) 
         {
             let card = {Value: cardValues[x], Suit: cardSuits[i]};
-            deck.push(card);
-
+            newDeck.push(card);
+            console.log(deck);
         }
     }
-    return deck;    
+    return newDeck;    
 }
+
+deck = getDeck();
+console.log(deck)
 
 
 //Shuffling the deck of cards
 function shuffleDeck()
 {
 	
-	for (i = 0; i < 500; i++)
+	for (let i = 0; i < 500; i++)
 	{
-		var location1 = Math.floor((Math.random() * deck.length));
-		var location2 = Math.floor((Math.random() * deck.length));
-		var tmp = deck[location1];
+		let holder1 = Math.floor((Math.random() * deck.length));
+		let holder2 = Math.floor((Math.random() * deck.length));
+		var temp = deck[holder1];
 
-		deck[location1] = deck[location2];
-		deck[location2] = tmp;
+		deck[holder1] = deck[holder2];
+		deck[holder2] = temp;
+    }
+}
+
+
+
+
+function renderDeck()
+{
+      $("#deck").innerHTML = "";
+
+	for(let i = 0; i < deck.length; i++)
+	{
+        let suit = document.createElement("div");
+		let card = document.createElement("div");
+		let value = document.createElement("div");
+        
+        suit.className = "suit " + deck[i].suit;
+		card.className = "card";
+		value.className = "value";
+		
+
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		$("#deck").appendChild(card);
 	}
 }
 
+*/
